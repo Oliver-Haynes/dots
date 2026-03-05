8bb65943dbf284a8d697bbcf5de93d94abf6b6cf
Oliver Haynes <132023324+Oliver-Haynes@users.noreply.github.com>
2026-03-05 14:13:59 +0000

    Add custom key mappings for scrolling in Neovim

---
+++ nvim/mappings.lua
@@ -0,0 +1,13 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+--map("n", ";", ":", { desc = "CMD enter command mode" })
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+
+map("n","<C-d>","<C-d>zz")
+map("n","<C-u>","<C-u>zz")
